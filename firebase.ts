b75e363951c9b4b072ca4bbbc4e91b392a34d9bf
Victor Mascarenhas <78654482+victor-mascarenhas@users.noreply.github.com>
2025-07-24 18:07:08 -0300

    config

--- firebase.ts
+++ firebase.ts
@@ -1,15 +1,13 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBPLPqQ8dMN44v81kQw52SbJ71VxwerhqY",
+  authDomain: "http://hackathon-farm.firebaseapp.com/",
+  projectId: "hackathon-farm",
+  storageBucket: "hackathon-farm.firebasestorage.app",
+  messagingSenderId: "81515145275",
+  appId: "1:81515145275:web:a96e4d21e690a387bad41e",
 };
-
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-
